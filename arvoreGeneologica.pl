541582b3522a2d88a8512c4227a713a83c6157e4
Creuma Kuzola <creumakuzola@gmail.com>
2020-12-07 23:07:29 +0100

    One more rule

--- arvoreGeneologica.pl
+++ arvoreGeneologica.pl
@@ -30,6 +30,7 @@ pai(X,Y):-progenitor(X,Y),sexo(X,masculino).
 irma(X,Y):-mae(Z,X),mae(Z,Y);pai(K,X),pai(K,Y),X\==Y,sexo(X,femenino).
 irmao(X,Y):-mae(Z,Y),mae(Z,Y);pai(K,Y),pai(K,Y),X\==Y,sexo(X,masculino).
 tio(X,Y):-progenitor(Z,Y),irmao(Z,X),sexo(X,masculino).
+primo(X,Y):-progenitor(Z,X),progenitor(K,Y),irmao(Z,K),sexo(X,masculino).
 
 
 

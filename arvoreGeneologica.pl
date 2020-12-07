98dc248d14a088a5670843bd1a881679db657922
Creuma Kuzola <creumakuzola@gmail.com>
2020-12-07 23:56:15 +0100

    New rules of the tree

--- arvoreGeneologica.pl
+++ arvoreGeneologica.pl
@@ -27,10 +27,16 @@ sexo(carlos,masculino).
 
 mae(X,Y):-progenitor(X,Y),sexo(X,femenino).
 pai(X,Y):-progenitor(X,Y),sexo(X,masculino).
+
+filho(X,Y):-progenitor(Y,X),sexo(X,masculino).
+filha(X,Y):-progenitor(Y,X),sexo(X,femenino).
+
 irma(X,Y):-mae(Z,X),mae(Z,Y);pai(K,X),pai(K,Y),X\==Y,sexo(X,femenino).
 irmao(X,Y):-mae(Z,Y),mae(Z,Y);pai(K,Y),pai(K,Y),X\==Y,sexo(X,masculino).
-tio(X,Y):-progenitor(Z,Y),irmao(Z,X),sexo(X,masculino).
+
 primo(X,Y):-progenitor(Z,X),progenitor(K,Y),irmao(Z,K),sexo(X,masculino).
+tio(X,Y):-progenitor(Z,Y),irmao(Z,X),sexo(X,masculino).
+
 
 
 
